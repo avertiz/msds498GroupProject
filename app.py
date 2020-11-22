58c0745deae6159bfba9eadda65490edc51533db
Andres Vertiz <andre.vertiz@gmail.com>
2020-11-22 05:52:26 +0000

    updading app

--- app.py
+++ app.py
@@ -3,29 +3,132 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 from google.cloud import bigquery
 import score
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/dZVMbK.css']
+
 client = bigquery.Client()
-app = dash.Dash(__name__)
 
+click_count = 0
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "LandingSpot"
 app.layout = html.Div([
-    
-    html.H1("LandingSpot", style = {'text-align': 'center'}),
-    html.H4("Where will you land?", style = {'text-align': 'center'}),
-    html.Br(),html.Br(),html.Br(),
 
-    html.Label(["How many bedrooms do you need?",
-        dcc.Dropdown(   id = 'bedrooms',
-                        options = [ {'label': '1', 'value': 1},
-                                    {'label': '2', 'value': 2},
-                                    {'label': '3', 'value': 3},
-                                    {'label': '4', 'value': 4}],
-                        style = {'width': '23%'}
-        )
-    ]),
+    html.H1("LandingSpot", style = {'text-align': 'center'}),
+    html.H5("Where will you land?", style = {'text-align': 'center'}),
+    html.Br(),  
+
+    # Preferences  
+    html.Div([
+        
+        html.H6("Tell me what you need...", style = {'text-align': 'left', 'padding': '5px'}),
+
+        # Bedrooms Button
+        html.Div([
+            html.Label(["Bedrooms:",
+                dcc.Dropdown(   id = 'bedrooms',
+                                options = [ {'label': '1', 'value': 1},
+                                            {'label': '2', 'value': 2},
+                                            {'label': '3', 'value': 3},
+                                            {'label': '4', 'value': 4}],
+                                style = {'width': '35%', 'display': 'inline-block', 'vertical-align': 'middle', 'padding': '5px'}
+                )
+            ])
+        ], style = {'width': '100%', 'padding': '5px'}),
+        
+        # Commute Button
+        html.Div([
+            html.Label(["How to do you want to get to work?",
+                dcc.Dropdown(   id = 'commute',
+                                options = [ {'label': 'Driving', 'value': 'Driving'},
+                                            {'label': 'Public Transportation', 'value': 'Public Transportation'},
+                                            {'label': 'Walking', 'value': 'Walking'},                                        
+                                            {'label': 'Other', 'value': 'Other'}],
+                                style = {'width': '50%', 'display': 'inline-block', 'vertical-align': 'middle', 'padding': '5px'}
+                )
+            ])
+        ], style = {'width': '100%', 'padding': '5px'}),
+
+        # Weather Button
+        html.Div([
+            html.Label(["What kind of weather do you like?",
+                dcc.Dropdown(   id = 'weather',
+                                options = [ {'label': 'Test1', 'value': 'Test1'},
+                                            {'label': 'Test2', 'value': 'Test2'}],
+                                style = {'width': '50%', 'display': 'inline-block', 'vertical-align': 'middle', 'padding': '5px'}
+                )
+            ])
+        ], style = {'width': '100%', 'padding': '5px'}),
+
+        # Diversity Button
+        html.Div([
+            html.Label(["Diversity level of the city population:",
+                dcc.Dropdown(   id = 'diversity',
+                                options = [ {'label': 'Higher', 'value': 'Higher'},
+                                            {'label': 'Lower', 'value': 'Lower'}],
+                                style = {'width': '50%', 'display': 'inline-block', 'vertical-align': 'middle', 'padding': '5px'}
+                )
+            ])
+        ], style = {'width': '100%', 'padding': '5px'}),
+
+        # Education Button
+        html.Div([
+            html.Label(["Education level of the city population:",
+                dcc.Dropdown(   id = 'education',
+                                options = [ {'label': 'Higher', 'value': 'Higher'},
+                                            {'label': 'Lower', 'value': 'Lower'}],
+                                style = {'width': '50%', 'display': 'inline-block', 'vertical-align': 'middle', 'padding': '5px'}
+                )
+            ])
+        ], style = {'width': '100%', 'padding': '5px'})
+    
+    ], style = {'width': '30%', 'border' : '2px solid DarkSlateGray'}),
+
+    # Ranking
+    html.Div([
+        html.H6("Tell me what you care about the most...", style = {'text-align': 'left', 'padding': '5px'}),
+
+        html.Label(["1st",            
+            dcc.Dropdown(   id = '1st',
+                            options = [ {'label': 'Commute', 'value': 'Commute'},
+                                        {'label': 'Weather', 'value': 'Weather'},
+                                        {'label': 'Diversity', 'value': 'Diversity'},
+                                        {'label': 'Education', 'value': 'Education'}],
+                            style = {'width': '50%', 'display': 'inline-block', 'vertical-align': 'middle', 'padding': '5px'}
+            )
+        ], style = {'padding': '5px'}),
+
+        html.Label(["2nd",            
+            dcc.Dropdown(   id = '2nd',
+                            options = [ {'label': 'Commute', 'value': 'Commute'},
+                                        {'label': 'Weather', 'value': 'Weather'},
+                                        {'label': 'Diversity', 'value': 'Diversity'},
+                                        {'label': 'Education', 'value': 'Education'}],
+                            style = {'width': '50%', 'display': 'inline-block', 'vertical-align': 'middle', 'padding': '5px'}
+            )
+        ], style = {'padding': '5px'}),
+
+        html.Label(["3rd",            
+            dcc.Dropdown(   id = '3rd',
+                            options = [ {'label': 'Commute', 'value': 'Commute'},
+                                        {'label': 'Weather', 'value': 'Weather'},
+                                        {'label': 'Diversity', 'value': 'Diversity'},
+                                        {'label': 'Education', 'value': 'Education'}],
+                            style = {'width': '50%', 'display': 'inline-block', 'vertical-align': 'middle', 'padding': '5px'}
+            )
+        ], style = {'padding': '5px'})
+
+    ], style = {'width': '30%', 'border' : '2px DodgerBlue solid'}),    
+
+    html.Div([
+        html.Button('Submit', 
+                id='submit-val', 
+                n_clicks = 0)
+    ], style={'horizontalAlign': 'middle', 'display':'flex', 'padding': '5px'}),
 
     html.Br(),
     
@@ -38,7 +141,7 @@ app.layout = html.Div([
     ),
 
     html.Br(),html.Br(),html.Br(),
-    
+
     html.Footer("Sources:")
 
 ])
@@ -48,17 +151,39 @@ app.layout = html.Div([
      Output(component_id='results_table', component_property='data'),
      Output(component_id='results_table', component_property='columns'),
      Output(component_id='results_table', component_property='style_table')],
-    [Input(component_id='bedrooms', component_property='value')]
+    [Input(component_id = 'submit-val', component_property = 'n_clicks')],
+    [State(component_id='bedrooms', component_property='value'),
+     State(component_id = 'commute', component_property = 'value'),
+     State(component_id = 'weather', component_property = 'value'),
+     State(component_id = 'diversity', component_property = 'value'),
+     State(component_id = 'education', component_property = 'value'),
+     State(component_id = '1st', component_property = 'value'),
+     State(component_id = '2nd', component_property = 'value'),
+     State(component_id = '3rd', component_property = 'value')]
 )
-def show_results_table(bedrooms_input):
-    if bedrooms_input is None:
-        raise PreventUpdate    
-    df = score.output_table(bedrooms = bedrooms_input, client = client)
-    cols = [{"name": 'City', "id": 'FormattedName'},
-            {"name": 'Score', "id": 'Score'}]
-    style_table = {'overflowX': 'scroll'}
-    header = "Looks like you're landing in {}!".format(df.iloc[0]['FormattedName'])
-    return(header, df.to_dict('records'), cols, style_table)
+def show_results_table(n_clicks, beds, commute, weather, diversity, education, first, second, third):
+    if n_clicks == 0:
+        raise PreventUpdate
+    elif beds is None or commute is None or weather is None or diversity is None or education is None or first is None or second is None or third is None:
+        header = "Please fill out the entire form"
+        df = pd.DataFrame()
+        cols = []
+        style_table = {'overflowX': 'scroll'}
+        return(header, df.to_dict('records'), cols, style_table)
+    else:
+        df = score.output_table(client = client, 
+                                bedrooms = beds, 
+                                div_preference = diversity,
+                                ed_preference = education, 
+                                comm_preference = commute, 
+                                first = first, 
+                                second = second,
+                                third = third)
+        cols = [{"name": 'City', "id": 'FormattedName'},
+                {"name": 'Score', "id": 'Score'}]
+        style_table = {'overflowX': 'scroll'}
+        header = "Looks like you're landing in {}!".format(df.iloc[0]['FormattedName'])
+        return(header, df.to_dict('records'), cols, style_table)
 
 if __name__ == '__main__':
     app.run_server(debug = True)
--- score.py
+++ score.py
@@ -22,7 +22,7 @@ def score_rentals(bedrooms, client):
     query = "SELECT city_id, city FROM `msds-498-group-project.landing_spot.cities`"
     cities = client.query(query).to_dataframe()
     rentals = get_rentals(bedrooms = bedrooms, client = client)
-    rentals['rental_score'] = rentals['square_feet'] / rentals['price'] * rentals['bedrooms']
+    rentals['rental_score'] = rentals['price'] / rentals['square_feet']  * rentals['bedrooms']
     scores = rentals[['site', 'rental_score']].groupby('site').mean().reset_index()
     scores = pd.merge(scores, cities, left_on = 'site', right_on = 'city')
     scores = scores[['city_id', 'rental_score']]
@@ -62,33 +62,127 @@ def score_income(client):
     scores = income_data[['city_id', 'income_score']].groupby('city_id').sum().reset_index()
     return(scores)
 
+###### Score Demographics ######
+
+# diversity
+def get_diversity(client):
+    query = """SELECT c.city_id, d.Diversity____ as diversity
+               FROM `msds-498-group-project.landing_spot.demographics` d
+               JOIN `msds-498-group-project.landing_spot.cities` c
+               ON d.Cities = c.city
+            """
+    results = client.query(query).to_dataframe()
+    return(results)
+
+def score_diversity(client, preference):
+    diversity_data = get_diversity(client = client)
+    if preference == 'Higher':
+        return(diversity_data)
+    elif preference == 'Lower':
+        diversity_data['diversity'] = -diversity_data['diversity']
+        return(diversity_data)
+
+# education
+def get_education(client):
+    query = """SELECT c.city_id, d.Education__Bachelor_s_degree_or_higher__ as education
+               FROM `msds-498-group-project.landing_spot.demographics` d
+               JOIN `msds-498-group-project.landing_spot.cities` c
+               ON d.Cities = c.city
+            """
+    results = client.query(query).to_dataframe()
+    return(results)
+
+def score_education(client, preference):
+    education_data = get_education(client = client)
+    if preference == 'Higher':
+        return(education_data)
+    elif preference == 'Lower':
+        education_data['education'] = -education_data['education']
+        return(education_data)
+
+# commute
+def get_commute(client, preference):
+    if preference == 'Driving':
+        query = """SELECT c.city_id, d.Commuting_To_Work__Car__truck__or_van___drove_alone__ as commute
+                   FROM `msds-498-group-project.landing_spot.demographics` d
+                   JOIN `msds-498-group-project.landing_spot.cities` c
+                   ON d.Cities = c.city
+                """
+        results = client.query(query).to_dataframe()
+        return(results)
+    elif preference == 'Public Transportation':
+        query = """SELECT c.city_id, d.Commuting_To_Work__Public_transportation__excluding_taxicab___ as commute
+                   FROM `msds-498-group-project.landing_spot.demographics` d
+                   JOIN `msds-498-group-project.landing_spot.cities` c
+                   ON d.Cities = c.city
+                """
+        results = client.query(query).to_dataframe()
+        return(results)
+    elif preference == 'Walking':
+        query = """SELECT c.city_id, d.Commuting_To_Work__Walked__ as commute
+                   FROM `msds-498-group-project.landing_spot.demographics` d
+                   JOIN `msds-498-group-project.landing_spot.cities` c
+                   ON d.Cities = c.city
+                """
+        results = client.query(query).to_dataframe()
+        return(results)
+    elif preference == 'Other':
+        query = """SELECT c.city_id, d.Commuting_To_Work__Other_means__ as commute
+                   FROM `msds-498-group-project.landing_spot.demographics` d
+                   JOIN `msds-498-group-project.landing_spot.cities` c
+                   ON d.Cities = c.city
+                """
+        results = client.query(query).to_dataframe()
+        return(results)    
+
+def score_commute(client, preference):
+    commute_data = get_commute(client = client, preference = preference)
+    return(commute_data)
+
 ###### Rank Everything ######
+
 def get_city_names(client):
     query = "SELECT city_id, FormattedName FROM `msds-498-group-project.landing_spot.cities`"
     results = client.query(query).to_dataframe()
     return(results)
 
-def get_z_scores(df):
+def normalize_scores(df):
     cols = df.columns
-    z_scores = pd.DataFrame(columns = cols)
+    normalized_scores = pd.DataFrame(columns = cols)
     for col in cols:
         if col not in ['city_id', 'FormattedName']:
-            z_scores[col] = (df[col] - df[col].mean())/df[col].std(ddof=0)
+            min_ = df[col].min()
+            max_ = df[col].max()
+            normalized_scores[col] = (df[col] - min_) / (max_ - min_) # (x_i−min(x)) / (max(x)−min(x))
         else:
-            z_scores[col] = df[col]
-    return(z_scores)
+            normalized_scores[col] = df[col]
+    return(normalized_scores)
+
+def add_weights(df, first, second, third):
+    col_dict = {'Commute':'commute',
+                'Weather':'weather',
+                'Diversity':'diversity',
+                'Education':'education'}
+    df[col_dict[first]] = df[col_dict[first]] * 3
+    df[col_dict[second]] = df[col_dict[second]] * 2
+    df[col_dict[third]] = df[col_dict[third]] * 1.25
+    return(df)
 
 def sum_rows(df):
     df['Score'] = df.drop(['city_id', 'FormattedName'], axis=1).sum(axis=1).round(3)
     return(df)
 
-def output_table(bedrooms, client):    
+def output_table(client, bedrooms, div_preference, ed_preference, comm_preference, first, second, third):    
     names = get_city_names(client = client)
     rentals = score_rentals(bedrooms = bedrooms, client = client)
     income = score_income(client = client)
-    data_frames = [names, rentals, income]
+    diversity = score_diversity(client = client, preference = div_preference)
+    education = score_education(client = client, preference = ed_preference)
+    commute = score_commute(client = client, preference = comm_preference)
+    data_frames = [names, rentals, income, diversity, education, commute]
     results = reduce(lambda left,right: pd.merge(left,right,on=['city_id'], how='outer'), data_frames)
-    results = get_z_scores(df = results)
+    results = normalize_scores(df = results)
+    results = add_weights(df = results, first = first, second = second, third = third)
     results = sum_rows(df = results)
     results.sort_values(by=['Score'], inplace=True, ascending=False)
     return(results[['FormattedName', 'Score']])
